13f12c2d68735c4f9accec9204a7541643712c15
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-04-03 14:03:03 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 int main() {
-  int arr[5] = {10,20,30,40,50};
-  int size = 5;
-  int index;
+    int arr[5] = {10, 20, 30, 40, 50};
+    int size = 5;
+    int index;
 
-  scanf("%d", &index);
 
-  if (scanf("%d", &index) != 1) {
-        printf("숫자만 입력 가능합니다.\n");
-        return 1;
+    if (scanf("%d", &index) != 1) {
+        return 1; 
     }
 
+   
     if (index < 0 || index >= size) {
-        printf("error\n"); 
+      
+        printf("error!\n"); 
+    } 
     else {
+    
         printf("%d\n", arr[index]);
     }
-  return 0;
+
+    return 0;
 }
